de8d6cb96967f53eaed4349f2caa5c2f33238e34
phxgg <stamatis.1702@gmail.com>
2022-05-12 09:23:59 +0300

    :D

--- app/src/main/java/com/example/planetgreece/MainActivity.java
+++ app/src/main/java/com/example/planetgreece/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -22,18 +23,19 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         User user = (User) intent.getSerializableExtra(LoginTabFragment.USER_OBJECT);
-        System.out.println(user.getEmail());
+//        System.out.println(user.getEmail());
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         getSupportFragmentManager().beginTransaction().replace(R.id.mainConstaint, new HomeFragment()).commit();
         bottomNavigationView.setSelectedItemId(R.id.navHome);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
                 switch (item.getItemId()){
                     case R.id.navHome:
-                        fragment = new HomeFragment();
+                        fragment = HomeFragment.newInstance(user);
                         break;
                     case R.id.navSaved:
                         fragment = new SavedFragment();
