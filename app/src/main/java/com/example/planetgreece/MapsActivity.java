8afc372c5388537fc556b13a00f67ef5beb02e67
XKostikiadis <xristoskostikiadis@gmail.com>
2022-05-27 01:13:35 +0300

    MAP

--- app/src/main/java/com/example/planetgreece/MapsActivity.java
+++ app/src/main/java/com/example/planetgreece/MapsActivity.java
@@ -1,34 +1,176 @@
 package com.example.planetgreece;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback , GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+    private FusedLocationProviderClient mLocationClient;
+    boolean isPermissionGranted;
+    FloatingActionButton fab;
+    GoogleMap mGoogleMap;
+    private int GPS_REQUEST_CODE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Set the layout file as the content view.
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
 
-        // Get a handle to the fragment and register the callback.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+//        fab = findViewById(R.id.fab);
+
+        checkMyPermission();
+        initMap();
+        mLocationClient = new FusedLocationProviderClient(this);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                getCurrentLocation();
+//            }
+//        });
+    }
+
+    private void initMap() {
+        if(isPermissionGranted){
+            if (isGpsEnable()){
+                SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+                supportMapFragment.getMapAsync(this);
+            }
+        }
+    }
+
+    private boolean isGpsEnable(){
+//        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+//        boolean providerEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+//        if(providerEnable){
+//           return true;
+//        }else{
+//            AlertDialog alertDialog = new AlertDialog.Builder(this)
+//                    .setTitle("GPS Permission")
+//                    .setMessage("GPS is required for this app to work. Please enable GPS")
+//                    .setPositiveButton("Yes",((dialogInterface, i) -> {
+//                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                        startActivityForResult(intent,GPS_REQUEST_CODE);
+//                    }))
+//                    .setCancelable(false)
+//                    .show();
+//        }
+        return true;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation(){
+        mLocationClient.getLastLocation().addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                Location location = task.getResult();
+                gotoLocation(location.getLatitude(),location.getLongitude());
+            }
+        });
+    }
+
+    private void gotoLocation(double latitude, double longitude) {
+        LatLng latLng = new LatLng(latitude,longitude);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,15);
+        mGoogleMap.moveCamera(cameraUpdate);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    private void checkMyPermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Toast.makeText(MapsActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                isPermissionGranted = true;
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package",getPackageName(),"");
+                intent.setData(uri);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        mGoogleMap.setMyLocationEnabled(true);
+        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng point) {
+                mGoogleMap.addMarker(new MarkerOptions().position(point).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
 
     }
 
-    // Get a handle to the GoogleMap object and display marker.
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(40.629269, 22.947412))
-                .title("Marker"));
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GPS_REQUEST_CODE){
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            boolean providerEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            if (providerEnable){
+                Toast.makeText(this, "GPS is enable", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "GPS in not enable", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
